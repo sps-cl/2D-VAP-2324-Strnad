984f5c4c66ef9738d03115edf11aa9edd8e7f3b1
MarekStrnad <marekstrnad55@gmail.com>
2023-09-12 22:11:28 +0200

    Added new program

---
+++ 01-Hodina/script.js
@@ -0,0 +1,11 @@
+function createConfetti() {
+    const confetti = document.createElement("div");
+    confetti.className = "confetti";
+    confetti.style.left = Math.random() * 100 + "vw";
+    document.body.appendChild(confetti);
+    setTimeout(() => {
+        confetti.remove();
+    }, 2000); // Čas, po kterém konfeta zmizí
+}
+
+setInterval(createConfetti, 200); // Interval pro vytváření konfet
